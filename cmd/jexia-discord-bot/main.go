caf2d078dbfcbe94c94b890d2806c92a2408aa53
Bailey Matthews <baileyjmatthews@gmail.com>
2020-04-08 17:02:35 +0100

    Feature: added logrus

--- cmd/jexia-discord-bot/main.go
+++ cmd/jexia-discord-bot/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"github.com/baileyjm02/jexia-discord-bot/internal/api"
+	"github.com/baileyjm02/jexia-discord-bot/internal/bot"
+	"github.com/baileyjm02/jexia-discord-bot/internal/pkg/logger"
 )
 
 func main() {
+	// Format the logger so we can import logrus and have a correct output in other areas
+	logger.Format()
+
 	// Start the API process, doing this starts the HTTP server
 	// for webhooks and connects the bot to Discord's gateway
-	api.Start()
+	bot.Start()
 }
